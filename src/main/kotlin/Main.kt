4651b0c0b629c42f0d023282da34cf910076a572
Lilranime <112871171+Lilranime@users.noreply.github.com>
2023-09-04 14:44:59 +0100

    Learn kotlin to build android apps

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
 fun main(args: Array<String>) {
-    print("Hello World!")
-    println("Hello, Kotlin")
+    val userName: String = "Tarila"
+    val userAge: Int = 16
+    val exampleRawString = """Florian
+        |Tarila
+        |
+    """.trimMargin()
+    print(exampleRawString)
+    print("Hello World! $userName. I am $userAge years old.")
 }
