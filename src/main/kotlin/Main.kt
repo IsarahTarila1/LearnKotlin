b104dea500cc2cf79d66baa5bd3a6bfeeef44510
Lilranime <112871171+Lilranime@users.noreply.github.com>
2023-09-04 16:09:20 +0100

    Printing out statements

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,31 @@
 fun main(args: Array<String>) {
+    //Assign variables
     var firstNumber = 8
     val secondNumber = 5
+    //Print result for the following arithmetic task
     println("First Number + Second  Number = ${firstNumber+secondNumber}")
+
     println("First Number - Second  Number = ${firstNumber-secondNumber}")
+
     println("First Number / Second  Number = ${firstNumber/secondNumber}")
+
     println("First Number % Second  Number = ${firstNumber%secondNumber}")
+
     println("First Number * Second  Number = ${firstNumber*secondNumber}")
+
+    //Printing out the statements
+    firstNumber+=2
+    println("first Number += 2 = $firstNumber")
+
+    firstNumber-=2
+    println("first Number -= 2 = $firstNumber")
+
+    firstNumber*=2
+    println("first Number *= 2 = $firstNumber")
+
+    firstNumber/=2
+    println("first Number /= 2 = $firstNumber")
+
+    firstNumber%=2
+    println("first Number %= 2 = $firstNumber")
 }
