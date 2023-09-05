fb4a062b610777dfa30b9082fea2430bd36c6d78
Lilranime <112871171+Lilranime@users.noreply.github.com>
2023-09-05 13:41:59 +0100

    Using a for loop to iterate through an array

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,24 +3,13 @@ fun main(args: Array<String>) {
     val names = arrayOf("Tarila", "David", "Isarah")
     val numbers = arrayOf(1, 2, 3)
 
-    //Assign values to multi-lines Array
-    val multipleIndex = arrayOf(arrayOf(4,5,6),
-    arrayOf(7,8,9),
-        arrayOf(10,11,12)
-    )
+    //Use a for loop to print the indexes and element
+    for (index in names.indices)
+        println("Index: $index Element: ${names[index]}")
 
-    //Print the size of elements in the array
-    println("Number of elements: ${names.size}")
-
-    //Print the element of the array
-    println("First element: ${names[0]}"+"\nSecond element: ${names[1]}"+"\nThird element: ${names[2]}")
-
-    //Print values of multi lines array
-    println("Second element of the second array: ${multipleIndex[1][1]}")
-
-    //print the third character of index 0
-    println("The third character of ${names[0]}: ${names[0][2]}")
-
-    //print out the numbers of characters of index 0
-    println("We have ${names[0].length} alphabets in ${names[0]}")
+    //Use a for loop to print a multiplication times table
+    for (i in 1 .. 12 )
+        println("2 x $i = ${i*2}")
+    for (i in 1..12)
+        println("3 x $i = ${i*3}")
 }
