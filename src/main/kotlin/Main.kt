3db1c148251b2139929a19ccf3ab63e022af1b92
Lilranime <112871171+Lilranime@users.noreply.github.com>
2023-09-05 09:53:49 +0100

    Arrays and its elements

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,26 @@
 fun main(args: Array<String>) {
-    //Assign variables
-    var firstNumber = 8
-    val secondNumber = 5
-    //Print result for the following arithmetic task
-    println("First Number + Second  Number = ${firstNumber+secondNumber}")
+    //Assign values to names array
+    val names = arrayOf("Tarila", "David", "Isarah")
+    val numbers = arrayOf(1, 2, 3)
 
-    println("First Number - Second  Number = ${firstNumber-secondNumber}")
+    //Assign values to multi-lines Array
+    val multipleIndex = arrayOf(arrayOf(4,5,6),
+    arrayOf(7,8,9),
+        arrayOf(10,11,12)
+    )
 
-    println("First Number / Second  Number = ${firstNumber/secondNumber}")
+    //Print the size of elements in the array
+    println("Number of elements: ${names.size}")
 
-    println("First Number % Second  Number = ${firstNumber%secondNumber}")
+    //Print the element of the array
+    println("First element: ${names[0]}"+"\nSecond element: ${names[1]}"+"\nThird element: ${names[2]}")
 
-    println("First Number * Second  Number = ${firstNumber*secondNumber}")
+    //Print values of multi lines array
+    println("Second element of the second array: ${multipleIndex[1][1]}")
 
-    //Printing out the statements
-    firstNumber+=2
-    println("first Number += 2 = $firstNumber")
+    //print the third character of index 0
+    println("The third character of ${names[0]}: ${names[0][2]}")
 
-    firstNumber-=2
-    println("first Number -= 2 = $firstNumber")
-
-    firstNumber*=2
-    println("first Number *= 2 = $firstNumber")
-
-    firstNumber/=2
-    println("first Number /= 2 = $firstNumber")
-
-    firstNumber%=2
-    println("first Number %= 2 = $firstNumber")
+    //print out the numbers of characters of index 0
+    println("We have ${names[0].length} alphabets in ${names[0]}")
 }
